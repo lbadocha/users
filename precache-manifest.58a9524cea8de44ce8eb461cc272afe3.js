ab446b8a148285dcb4b5c159ee8acfd8eabb95fe
lukaszbadocha <krakow@akademia108.pl>
2019-11-05 16:41:03 +0100

    Updates

---
+++ precache-manifest.58a9524cea8de44ce8eb461cc272afe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57fcd569558b5abc2aadfbaa5d376d9",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "b3dd6026167f4e3ba936",
+    "url": "/users/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f987ba54d7f8a7512792",
+    "url": "/users/static/js/2.6edbfc48.chunk.js"
+  },
+  {
+    "revision": "b3dd6026167f4e3ba936",
+    "url": "/users/static/js/main.4cffc311.chunk.js"
+  },
+  {
+    "revision": "1fe9f15e4f2571e75ea0",
+    "url": "/users/static/js/runtime-main.8ecbba6d.js"
+  }
+]);
